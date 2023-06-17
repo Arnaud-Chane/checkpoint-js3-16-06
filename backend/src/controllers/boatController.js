0711fc34ac7d2f08f94ae1c8116b9122c1f8b615
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-06-17 20:46:20 +0200

    creating boat controller & updating tile controller :: replace all boat words to tile

--- backend/src/controllers/boatController.js
+++ backend/src/controllers/boatController.js
@@ -1,17 +1,17 @@
 const models = require("../models");
 
-const getAllTiles = (req, res) => {
-  models.tile
+const getAllBoats = (req, res) => {
+  models.boat
     .findAll()
     .then((results) => {
       res.status(200).send(results);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Pb to retreive all the tiles");
+      res.status(500).send("Pb to retrieve all the boats");
     });
 };
 
 module.exports = {
-  getAllTiles,
+  getAllBoats,
 };
