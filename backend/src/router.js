35f6bf915c86a98e02ddd84d337aba2371560d37
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-06-17 20:38:59 +0200

    Adding tileController to router.js & setting route for tile

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const tileController = require("./controllers/tileController");
+
+router.get("/tiles", tileController.getAllBoats);
+
 module.exports = router;
